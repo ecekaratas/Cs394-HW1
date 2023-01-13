daed384680dce621f080cb947d5a203353d5ce34
Ece Karataş <ece.karatas@ozu.edu.tr>
2023-01-13 20:44:23 +0300

    settings half, profile no

--- app/src/main/java/com/example/recyclerviewexinclass/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewexinclass/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.profile)
         //navController = findNavController(R.id.navigation)
     }
 
