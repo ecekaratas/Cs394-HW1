11ad343477564a69d57d51d1db6c785dbef14d00
Ece Karataş <ece.karatas@ozu.edu.tr>
2023-01-14 18:59:00 +0300

    for ed to work on

--- app/src/main/java/com/example/recyclerviewexinclass/database/list/ListAdapter.kt
+++ app/src/main/java/com/example/recyclerviewexinclass/database/list/ListAdapter.kt
@@ -9,9 +9,50 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerviewexinclass.R
 import com.example.recyclerviewexinclass.database.Comment
+import com.example.recyclerviewexinclass.databinding.CustomRowBinding
+import com.example.recyclerviewexinclass.databinding.FragmentListBinding
+import com.example.recyclerviewexinclass.databinding.FragmentListBindingImpl
 import kotlinx.android.synthetic.main.custom_row.view.*
 
 
+class ListAdapter: ListAdapter<Comment, ListAdapter.MyViewHolder>(MyListDiffCallBack()){
+
+    class MyViewHolder(val binding: CustomRowBinding): RecyclerView.ViewHolder(binding.root) {
+
+        val commentUID: TextView = binding.commentUID
+
+        companion object{
+            fun from(parent: ViewGroup): MyViewHolder{
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = CustomRowBinding.inflate(layoutInflater, parent, false)
+                return MyViewHolder(binding)
+            }
+        }
+        fun bind(comment: Comment){
+            commentUID.text = comment.uid.toString()
+            binding.comment = comment
+        }}
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder{
+            return MyViewHolder.from(parent)
+        }
+        override fun onCreateViewHolder(holder: MyViewHolder, position: Int){
+            val currentItem = getItem(position)
+            holder.bind(currentItem)
+        }
+    }
+
+    class MyListDiffCallBack : DiffUtil.ItemCallback<Comment>(){
+        override fun areItemsTheSame(oldItem: Comment, newItem: Comment): Boolean {
+            return oldItem.uid == newItem.uid
+        }
+
+        override fun areContentsTheSame(oldItem: Comment, newItem: Comment): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
+/*
 class ListAdapter: RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
 
     private var commentList = emptyList<Comment>()
@@ -38,4 +79,4 @@ class ListAdapter: RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
         this.commentList = comment
         notifyDataSetChanged()
     }
-}
+}*/
