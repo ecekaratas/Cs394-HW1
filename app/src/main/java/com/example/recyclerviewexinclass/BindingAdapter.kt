767e8b3e68865b0674513d410b630fd0be7aeef1
Ece Karataş <ece.karatas@ozu.edu.tr>
2023-01-14 19:43:27 +0300

    ab ++ data no

--- app/src/main/java/com/example/recyclerviewexinclass/BindingAdapter.kt
+++ app/src/main/java/com/example/recyclerviewexinclass/BindingAdapter.kt
@@ -8,9 +8,9 @@ import com.example.recyclerviewexinclass.database.Comment
 fun ImageView.setPlantImage(item: Comment){
     setImageResource(
         when (item.uid.toInt() % 8) {
-            0 -> R.drawable.r0
+            0 -> R.drawable.r2
             1 -> R.drawable.r1
-            2 -> R.drawable.r2
+            2 -> R.drawable.r0
             3 -> R.drawable.r3
             4 -> R.drawable.r4
             5 -> R.drawable.r5
